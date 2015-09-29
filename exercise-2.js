af5b6f03337ebda00425e49c9eb55a89af2c8ddf
Nora Top <top.nora@gmail.com>
2015-09-29 17:46:33 +0000

    exercise-2 ready for review

---
+++ exercise-2.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello/:firstName', function (req, res) {
+  //console.log(req);
+  //console.log(res);
+  res.send('<h1>Hello '+req.params.firstName+'!</h1>');
+});
+
+
+
+/* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
+
+// Boilerplate code to start up the web server
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
