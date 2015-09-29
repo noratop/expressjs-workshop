a419f098d34638d196211047986545750170e36c
Nora Top <top.nora@gmail.com>
2015-09-29 20:53:01 +0000

    finalized findEntry function

--- exercise-5.js
+++ exercise-5.js
@@ -32,71 +32,48 @@ var entries = {
 
 function findEntry(firstName,lastName,email){
   var results = [];
-  var firstName = firstName || '';
-  var lastName = lastName || '';
-  var email = email || '';
-  
+
+  //console.log(firstName +' '+ lastName +' '+ email);
   
   Object.keys(entries).forEach(function(key) {
+
       var e = entries[key];
-      if (e.firstName.indexOf(firstName) >= 0 || e.lastName.indexOf(lastName) >= 0) {
+
+      if (e.firstName.toLowerCase().indexOf(firstName.toLowerCase()) >= 0 || e.lastName.toLowerCase().indexOf(lastName.toLowerCase()) >= 0) {
+
           results.push(e);
+
       }
       else {
-          Object.keys(e.emails).some(function(emailKey) {
-              var email = e.emails[emailKey];
-              if (email.emailAddress.indexOf(email) >= 0) {
-                  results.push(e);
-                  return true;
-              }
+        e.emails.forEach(function(eObj){
+          Object.keys(eObj).some(function(emailKey) {
+
+            var emailadresses = eObj[emailKey];
+
+            if (emailadresses.toLowerCase().indexOf(email.toLowerCase()) >= 0) {
+              results.push(e);
+              return true;
+            }
           });
+        });
       }
   });
   return results;
 }
 
 
-// findEntriesByName: function(name) {
-//         var ab = this;
-//         var results = [];
-//         Object.keys(ab.entries).forEach(function(key) {
-//             var e = ab.entries[key];
-//             if (e.firstName.indexOf(name) >= 0 || e.lastName.indexOf(name) >= 0) {
-//                 results.push(e);
-//             }
-//             else {
-//                 Object.keys(e.emails).some(function(emailKey) {
-//                     var email = e.emails[emailKey];
-//                     if (email.emailAddress.indexOf(name) >= 0) {
-//                         results.push(e);
-//                         return true;
-//                     }
-//                 });
-//             }
-//         });
-//         return results;
-//     }
-
-
-
-
-
-
 
 
 app.get('/entry/search', function (req, res) {
-  //console.log(req);
-  //console.log(res);
-  try {
-      console.log();
-      //res.json(getEntry(req.params.entryId));
-       res.json(findEntry(req.query.firstName,req.query.lastName,req.query.email));
 
+  try {
+      res.json(findEntry(req.query.firstName,req.query.lastName,req.query.email));
   }
   catch(e){
-      res.status(404).send(e.message);
+      res.send(e.message);
       console.log(e);
   }
+  
 });
 
 
